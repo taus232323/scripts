0636a14293c04a36c5b14ac2f2eec4b1dd1350b1
taus232323 <123715048+taus232323@users.noreply.github.com>
2024-11-13 19:32:56 +0300

    Update keynopass

--- keynopass.sh
+++ keynopass.sh
@@ -5,6 +5,6 @@ chmod 600 ~/.ssh/authorized_keys
 sed -i.bak 's/^#\?PasswordAuthentication .*/PasswordAuthentication no/' /etc/ssh/sshd_config
 sed -i.bak 's/^#\?PubkeyAuthentication .*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 
-sudo systemctl restart sshd
+sudo systemctl restart ssh
 
 echo "Настройки SSH обновлены и сервер перезапущен."
